e7900f06e5df40b27167483d1c9038356c8fbcdf
Renan Veronez Drechsler <renanvz@outlook.com>
2021-06-04 12:33:02 -0300

    Grid

--- src/components/homeMoradas.tsx
+++ src/components/homeMoradas.tsx
@@ -2,12 +2,27 @@ import styles from './homeMoradas.module.scss';
 
 export function HomeMoradas() {
   return (
-    <div className={styles.structureContainer}>
-      <div className={styles.homeMoradas}>
-        <span>Conheça as opções que temos para você:</span>
-        <img src="/grid1.jpg" alt="Pé Grande1" />
-        <img src="/grid2.jpg" alt="Pé Grande2" />
-        <img src="/grid3.jpg" alt="Pé Grande3" />
+    <div className={styles.homeMoradas}>
+      <span>Conheça as opções que temos para você:</span>
+      <div className={styles.grid}>
+        <div className={styles.blockPhoto}>
+          <img src="/grid1.jpg" alt="Pé Grande1" />
+          <span className={styles.add}>Minilofts Pé Grande</span>
+          <span className={styles.addDown}>De frente pro mar, com piscina</span>
+          <span className={styles.saibaMais}>Saiba mais...</span>
+        </div>
+        <div className={styles.blockPhoto}>
+          <img src="/grid2.jpg" alt="Pé Grande2" />
+          <span className={styles.add}>Minilofts Pé Grande</span>
+          <span className={styles.addDown}>De frente pro mar, com piscina</span>
+          <span className={styles.saibaMais}>Saiba mais...</span>
+        </div>
+        <div className={styles.blockPhoto}>
+          <img src="/grid3.jpg" alt="Pé Grande3" />
+          <span className={styles.add}>Minilofts Pé Grande</span>
+          <span className={styles.addDown}>De frente pro mar, com piscina</span>
+          <span className={styles.saibaMais}>Saiba mais...</span>
+        </div>
       </div>
     </div>
   );
